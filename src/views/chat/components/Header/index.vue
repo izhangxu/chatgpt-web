04ac75f47940d75eddc05fe63694492883b758ce
shanezhangxu <shanezhangxu@didiglobal.com>
2024-10-20 01:24:42 +0800

    update

--- src/views/chat/components/Header/index.vue
+++ src/views/chat/components/Header/index.vue
@@ -18,7 +18,7 @@ const appStore = useAppStore()
 const chatStore = useChatStore()
 
 const collapsed = computed(() => appStore.siderCollapsed)
-const currentChatHistory = computed(() => chatStore.getChatHistoryByCurrentActive)
+const currentChatHistory = computed(() => chatStore.getHistoryByCurrentActive)
 
 function handleUpdateCollapsed() {
   appStore.setSiderCollapsed(!collapsed.value)
